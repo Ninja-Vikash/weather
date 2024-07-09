28a77ef8f8ef3bec91490eeefbe9a93fd5026aee
Vikash Kumar <vikashloharjsr@gmail.com>
2024-07-09 22:51:12 +0530

    footer and random background effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Forecast from "./pages/Forecast/Forecast";
 
 import Navbar from "./components/Navbar/Navbar";
+import { useState } from "react";
 
 function App() {
+  const [background, setBackground] = useState("1")
+  const theme = (data)=> {
+    let bgImage = Math.ceil(Math.random() * 9)
+    console.log(bgImage)
+    setBackground(`${bgImage}`)
+  }
+
+  const style = {
+    backgroundImage: `url("/img/${background}.avif")`
+  }
+
   return (
     <BrowserRouter>
-      <main>
-        <Navbar />
+      <main style={style}>
+        <Navbar theme={theme}/>
 
         <Routes>
           <Route index element={<Home />} />
-          <Route path="forecast" element={<Forecast />} />
+          {/* <Route path="forecast" element={<Forecast />} /> */}
           {/* <Route path="*" element={<NoPage />} /> */}
         </Routes>
+
+        <footer>
+          <div className="content container">
+            <p>&copy; Copyright reserved | Vikash Kumar</p>
+          </div>
+        </footer>
       </main>
     </BrowserRouter>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import "./Navbar.css"
-import { Link } from 'react-router-dom'
 
-function Navbar() {
+function Navbar({theme}) {
   return (
     <nav className='container'>
       <h2 className="nav_logo">WeatherApp.Inc</h2>
 
       <div className="navigation_area">
-        <h3><Link to="/">Home</Link></h3>
-        <h3><Link to="forecast">Forecast</Link></h3>
+        {/* <h3><Link to="/">Home</Link></h3>
+        <h3><Link to="forecast">Forecast</Link></h3> */}
+        <button onClick={()=> theme()}>♾️</button>
       </div>
     </nav>
   )
