54dee22bcddc3ac05d7579089af3b0d3dfcc4e22
Vikash Kumar <vikashloharjsr@gmail.com>
2024-07-10 20:27:17 +0530

    Background loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 
 import Navbar from "./components/Navbar/Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { preloadImages } from "./utils/preloadImage";
+
+
 
 function App() {
+  // Background images state
   const [background, setBackground] = useState("1")
+
+  useEffect(() => {
+    // Preload images
+    const imageUrls = Array.from({ length: 9 }, (_, i) => `/img/${i + 1}.avif`);
+    preloadImages(imageUrls);
+    console.log(imageUrls)
+  }, []);
+
+  // Function for setting a random background image
   const theme = (data)=> {
     let bgImage = Math.ceil(Math.random() * 9)
     console.log(bgImage)
